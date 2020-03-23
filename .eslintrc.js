13e6bcca1fef2fa93020d83f61641b9395082c93
lenkly <lenk.lydia@gmail.com>
2020-03-23 12:10:04 +0100

    Change eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,16 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
   },
   plugins: "prettier",
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "prettier"],
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2018,
   },
-  rules: {}
+  rules: {},
 };
